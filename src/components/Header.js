c20c92382bdd429c547354b1cd0ebb7a6d91b6a6
reireynoso <reireynoso@gmail.com>
2020-02-11 11:40:29 -0500

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Header = () => {
+    return(
+        <div style={{backgroundColor: "black"}}>
+            <div class="ui inverted segment">
+                <div class="ui inverted secondary pointing menu">
+                    <div className="ui inverted button">
+                        View DVDs
+                    </div>
+                    <div className="ui inverted button">
+                        
+                    </div>
+                    <div className="ui inverted button">
+                        Add New Photo
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Header
