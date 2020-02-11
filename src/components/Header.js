6267ff71323b7015a87e3b1f785a9540936cb2e1
reireynoso <reireynoso@gmail.com>
2020-02-11 17:53:26 -0500

    changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,15 +5,22 @@ const Header = () => {
         <div style={{backgroundColor: "black"}}>
             <div class="ui inverted segment">
                 <div class="ui inverted secondary pointing menu">
-                    <div className="ui inverted button">
-                        View DVDs
-                    </div>
-                    <div className="ui inverted button">
-                        
-                    </div>
-                    <div className="ui inverted button">
-                        Add New Photo
-                    </div>
+                    {/* <div style={{display: "flex"}}> */}
+                        <div className="left item" style={{justifyContent: "space-between"}}>
+                            <div className="ui inverted button">
+                                Block-Flix
+                            </div>
+                            <div className="ui inverted button">
+                                View DVDs
+                            </div>
+                        </div>
+                        <div className="right item">
+                            <div className="ui inverted button">
+                                Add New Photo
+                            </div>
+                        </div>
+                    {/* </div> */}
+                   
                 </div>
             </div>
         </div>
