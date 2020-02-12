87036674807bebc31c0d98b5f3a5cc6a2c94d6a3
reireynoso <reireynoso@gmail.com>
2020-02-12 12:27:43 -0500

    removed semantic and generate pure css

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,25 +3,20 @@ import React from 'react'
 const Header = () => {
     return(
         <div style={{backgroundColor: "black"}}>
-            <div class="ui inverted segment">
-                <div class="ui inverted secondary pointing menu">
-                    {/* <div style={{display: "flex"}}> */}
-                        <div className="left item" style={{justifyContent: "space-between"}}>
-                            <div className="ui inverted button">
-                                Block-Flix
-                            </div>
-                            <div className="ui inverted button">
-                                View DVDs
-                            </div>
-                        </div>
-                        <div className="right item">
-                            <div className="ui inverted button">
-                                Add New Photo
-                            </div>
-                        </div>
-                    {/* </div> */}
-                   
+            {/* left */}
+            <div>
+                <div>
+                    Logo
                 </div>
+
+                <div>
+                    DVDs
+                </div>
+            </div>
+
+            {/* right */}
+            <div>
+                Cart
             </div>
         </div>
     )
