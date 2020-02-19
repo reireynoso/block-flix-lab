6a012ce711751f8a260e1d8e49bcba64d6d695f6
reireynoso <reireynoso@gmail.com>
2020-02-19 17:55:01 -0500

    css changes

--- src/components/CartContainer.js
+++ src/components/CartContainer.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
+import ItemCardSimplified from './ItemCardSimplfied'
 
 export default class CartContainer extends Component {
   render() {
     return (
-      <div className="e-commerce-container"> CartContainer </div>
+      <div className="cart-container">
+          {
+              this.props.items.map(item => <ItemCardSimplified item={item}/>)
+          }
+      </div>
     );
   }
 }
--- src/components/ItemCardSimplfied.js
+++ src/components/ItemCardSimplfied.js
@@ -3,7 +3,10 @@ import React, { Component } from 'react';
 export default class ItemCardSimplified extends Component {
   render() {
     return (
-      <div className="item-card-simplified"> ItemCardSimplified </div>
+      <div className="item-card-simplified">
+          <p>{this.props.item.name}</p>
+          <button className="item-card-simplified-button">Remove from Cart</button>
+      </div>
     );
   }
 }
