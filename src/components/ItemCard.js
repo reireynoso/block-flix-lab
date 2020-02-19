6ed6484f8e7283a12f8c793b980552cc1f96addc
reireynoso <reireynoso@gmail.com>
2020-02-19 15:32:46 -0500

    css update

--- src/components/ItemCard.js
+++ src/components/ItemCard.js
@@ -1,9 +1,23 @@
 import React from 'react'
 
-const ItemCard = () => {
+const ItemCard = (props) => {
     return(
         <div className="item-card">
-            ItemCard
+            <h2>{props.item.name}</h2>
+            <img className="item-image" src={props.item.image_url}/>
+            <p>{props.item.description}</p>
+            <p>Price: ${props.item.price}</p>
+            <button className="item-button">
+                <div>
+                    <i className="material-icons">
+                        shopping_cart
+                    </i>
+                </div>
+                
+                <div>
+                    Add to Cart
+                </div>
+            </button>
         </div>
     )
 }
