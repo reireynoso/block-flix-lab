8f7c6b96b0e840f623d8a7128efe72c3b3e62317
reireynoso <reireynoso@gmail.com>
2020-02-19 18:02:50 -0500

    changes

--- src/components/ItemCardSimplfied.js
+++ src/components/ItemCardSimplfied.js
@@ -4,6 +4,7 @@ export default class ItemCardSimplified extends Component {
   render() {
     return (
       <div className="item-card-simplified">
+          <img className="item-card-simplified-image" src={this.props.item.image_url}/>
           <p>{this.props.item.name}</p>
           <button className="item-card-simplified-button">Remove from Cart</button>
       </div>
