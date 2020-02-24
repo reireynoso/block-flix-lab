242f53f78e00f0b16d01d0ba5ce1d74844ab6509
reireynoso <reireynoso@gmail.com>
2020-02-23 22:01:00 -0500

    item card refactored

--- src/components/ItemCard.js
+++ src/components/ItemCard.js
@@ -2,40 +2,23 @@ import React from 'react'
 
 const ItemCard = (props) => {
     const {cart, item, item: {name, image_url, description, price}, handleAddToCart} = props
+    
     return(
         <div className="item-card">
             <h2>{name}</h2>
             <img className="item-image" src={image_url} alt={name}/>
             <p>{description}</p>
             <p>Price: ${price}</p>
-            {
-                cart.includes(item) ? 
+            <button disabled={cart.includes(item)} onClick={() => handleAddToCart(props.item)} className="item-button">
                 <div>
-                    <button disabled className="item-button">
-                        <div>
-                            <i className="material-icons">
-                                shopping_cart
-                            </i>
-                        </div>      
-                        <div>
-                            Already in Cart
-                        </div>
-                    </button> 
-                </div>
-                : 
+                    <i className="material-icons">
+                        shopping_cart
+                    </i>
+                </div>      
                 <div>
-                     <button onClick={() => handleAddToCart(props.item)} className="item-button">
-                        <div>
-                            <i className="material-icons">
-                                shopping_cart
-                            </i>
-                        </div>      
-                        <div>
-                            Add to Cart
-                        </div>
-                    </button>
+                    {cart.includes(item) ? "Already in Cart" : "Add to Cart"} 
                 </div>
-            }    
+            </button>    
         </div>
     )
 }
